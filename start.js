abaec1a18cfb6013d7f163a0f5baac0f527e3080
Teerapat Chintanakit <t.chintanakit@gmail.com>
2020-12-09 02:31:40 +0700

    Extract getTimeLeft function

--- start.js
+++ start.js
@@ -8,17 +8,28 @@ const setElemInnerText = (id, text) => {
   elem.innerText = text
 }
 
-const countDown = () => {
+const getTimeLeft = () => {
   const thisYear = new Date().getFullYear()
   const now = Date.now()
   const newYear = new Date(0, 11, 31, 23, 59, 59).setFullYear(thisYear)
   const timeLeft = newYear - now
+  return {
+    nextYear: thisYear + 1,
+    days: Math.floor(timeLeft / DAY),
+    hours: Math.floor((timeLeft % DAY) / HOUR),
+    minutes: Math.floor((timeLeft % HOUR) / MINUTE),
+    seconds: Math.floor((timeLeft % MINUTE) / SECOND),
+  }
+}
 
-  setElemInnerText('title', `Countdown to Year ${thisYear + 1}`)
-  setElemInnerText('days', Math.floor(timeLeft / DAY))
-  setElemInnerText('hours', Math.floor((timeLeft % DAY) / HOUR))
-  setElemInnerText('minutes', Math.floor((timeLeft % HOUR) / MINUTE))
-  setElemInnerText('seconds', Math.floor((timeLeft % MINUTE) / SECOND))
+const countDown = () => {
+  const { nextYear, days, hours, minutes, seconds } = getTimeLeft()
+
+  setElemInnerText('title', `Countdown to Year ${nextYear}`)
+  setElemInnerText('days', days)
+  setElemInnerText('hours', hours)
+  setElemInnerText('minutes', minutes)
+  setElemInnerText('seconds', seconds)
 }
 
 const run = () => {
